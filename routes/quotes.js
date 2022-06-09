43b52db47e31befd5cf3f560adedc1ae3d972994
Felipe M <felipei.mancilla@gmail.com>
2022-06-09 17:16:12 -0400

    update - console

--- routes/quotes.js
+++ routes/quotes.js
@@ -15,7 +15,7 @@ router.post("/", async function (req, res, next) {
   try {
     res.json(await quotes.create(req.body));
   } catch (error) {
-    console.log("Error mientras se realizaba la consulta", error.message);
+    console.log("Error mientras se realizaba el insert", error.message);
     next(error);
   }
 });
